d25d6199078474db4a91421d8a8f78c782dba7a0
Danilonbs <danilo_eu_sk8@hotmail.com>
2023-03-23 20:07:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -15,6 +15,9 @@ function comecarEtapa() {
     let numeroHTML = '';
 
     for(let i=0; i<etapa.numeros;i++) {
+        if(i === 0) {
+            numeroHTML += '<div class="numero pisca"></div>';
+        } else
         numeroHTML += '<div class="numero"></div>';
     }
 
@@ -27,13 +30,42 @@ function comecarEtapa() {
 }
 
 function atualizaInterface() {
+    let etapa = etapas[etapaAtual];
+    let candidato = etapa.candidatos.filter((item) => {
+        if(item.numero === numero) {
+            return true;
+        } else {
+            return false;
+        }
+    });
 
+    if(candidato.length > 0) {
+        candidato = candidato[0];
+        seuVotoPara.style.display ='block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = `Nome: ${candidato.nome}</br>Partido: ${candidato.partido}`;
+
+        let fotosHtml = '';
+        for(let i in candidato.fotos) {
+       fotosHtml += `<div class="d-1-image small"><img src="images/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda}</div>`;
+        }
+        lateral.innerHTML = fotosHtml;
+    }
+    console.log("candidato", candidato);
 }
 
 function clicou(n) {
-    let numero = document.querySelector('.numero.pisca');
+    let elNumero = document.querySelector('.numero.pisca');
     if(numero !== null) {
-        numero.i
+        elNumero.innerHTML = n;
+        numero = `${numero}${n}`;
+
+        elNumero.classList.remove('pisca');
+        if(elNumero.nextElementSibling !== null){
+            elNumero.nextElementSibling.classList.add('pisca');
+        } else {
+            atualizaInterface();
+        }
     } 
 }
 
